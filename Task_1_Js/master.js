f6157e2571c74dfec24617d981bce885ed99bfa1
Soad-Elabd88 <soadelabd8@gmail.com>
2023-09-21 20:01:18 +0300

    javascript task

---
+++ Task_1_Js/master.js
@@ -0,0 +1,232 @@
+// Task(1) Less than 100
+
+// var num1 = +prompt("Enter num1");
+// var num2 = +prompt("Enter num2");
+
+// function lessThan100(num1, num2) {
+//     if (num1 + num2 < 100)
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(lessThan100(num1, num2));
+
+
+
+// Task(2) Buggy code (part5)
+
+// var num =+prompt("Enter num");
+// function printArray(num)
+// {
+//     var arr=[];
+//     for(var i=0 ;i<num ;++i)
+//     {
+//     arr[i] =i+1;
+//     }
+//     console.log(arr);
+// }
+// printArray(num);
+
+
+
+
+// Task(3) Buggy code part(7)
+
+// var num1 = +prompt("Enter num1");
+// var num2 = +prompt("Enter num2");
+
+// function swap(num1, num2) {
+//     var temp = num1;
+//     num1 = num2;
+//     num2 = temp;
+//     console.log(num1 + " " + num2);
+// }
+
+// swap(num1, num2);
+
+
+// Task(4) The Farm Problem
+
+
+// var chicken = +prompt("Enter chicken");
+// var cows = +prompt("Enter cows");
+// var pigs = +prompt("Enter pigs");
+
+// function animals(chicken, cows, pigs) {
+//     console.log(chicken*2+cows*4+pigs*4);
+// }
+
+// animals(chicken, cows, pigs);
+
+
+
+// Task(5) Using the "&&" operator
+
+
+// function andOperator(a, b) {
+//     if (a && b)
+//     return true;
+//     else {
+//     return false;
+//     }
+// }
+
+// console.log(andOperator(true,true));
+
+
+
+// Task(6) Are the number equal?
+
+
+// var num1 = +prompt("Enter num1");
+// var num2 = +prompt("Enter num2");
+
+// function equal(num1, num2) {
+//     if (num1 == num2)
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(equal(num1, num2));
+
+
+
+// Task(7) Football points
+
+// var wins = +prompt("Enter wins");
+// var draws = +prompt("Enter draws");
+// var losses = +prompt("Enter losses")
+
+// function footballPoints(wins, draws, losses) {
+//     return (wins*3 + draws*1 + losses*0)
+// }
+
+// console.log(footballPoints(wins, draws, losses));
+
+
+
+// Task(8) Convert Hours and Minutes into Seconds
+
+// var hours = +prompt("Enter hours");
+// var Minutes = +prompt("Enter minutes");
+
+// function Convert(hours, minutes) {
+//     return(hours*60*60 + minutes*60)
+// }
+
+// console.log(Convert(hours, Minutes));
+
+
+
+// Task(9) Fix the Expression
+
+// var num = +prompt("Enter num");
+
+// function isSeven(num) {
+//     if (num == 7)
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(isSeven(num));
+
+
+// Task(10) Equality Check
+
+
+// function equalityCheck(input1, input2) {
+//     if (input1  === input2 )
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(equalityCheck(1,"1" ));
+
+
+// Task(11) Profitable Gamble
+
+
+// var prop = 0.2;
+// var prize = 50;
+// var pay = 9;
+
+// function profitableGamble(prop, prize, pay) {
+//     if (prop * prize > pay)
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(profitableGamble(prop, prize, pay));
+
+
+
+// Task(12) Boolean to String Conversion
+
+
+// function boolToString(a) {
+//     if (a)
+//         return "true";
+//     else {
+//         return "false"
+//     }
+// }
+
+// console.log(boolToString(false));
+
+
+// Task(13) Using Arrow Functions
+
+// function arrowFunc(a)
+// {
+//     return a;
+// }
+// console.log(arrowFunc(3));
+// console.log(arrowFunc("3"));
+// console.log(arrowFunc(true));
+
+
+
+// Task(14) Frames Per Second
+
+// var Frames = 1;
+// var Minutes = 1;
+
+// function fps(frames, Minutes) {
+//     return (frames*Minutes*60)
+// }
+
+// console.log(fps(Frames, Minutes));
+
+
+
+// Task(15) Miserable Parody of a Calculator
+
+// var num1 = 23;
+// var num2 = 4;
+// var text = "num1+num2";
+// var result = eval(text);
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
