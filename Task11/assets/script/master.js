aad086377ae1afd25db6fe61ef0dde89ed5ceb7a
Soad-Elabd88 <soadelabd8@gmail.com>
2023-10-19 23:21:05 +0300

    {Task11}

--- Task11/assets/script/master.js
+++ Task11/assets/script/master.js
@@ -1,12 +1,66 @@
-const alertContainer = document.querySelector(".alert-container");
-const sendBtn = document.querySelector(".send");
-const okBtn = document.querySelector(".ok");
-sendBtn.addEventListener("click", () => {
-alertContainer.style.display = "flex";
-});
-okBtn.addEventListener("click", () => {
-alertContainer.style.display = "none";
-});
-alertContainer.addEventListener("click", () => {
-alertContainer.style.display = "none";
-});
+// const alertContainer = document.querySelector(".alert-container");
+// const sendBtn = document.querySelector(".send");
+// const okBtn = document.querySelector(".ok");
+// sendBtn.addEventListener("click", () => {
+// alertContainer.style.display = "flex";
+// });
+// okBtn.addEventListener("click", () => {
+// alertContainer.style.display = "none";
+// });
+// alertContainer.addEventListener("click", () => {
+// alertContainer.style.display = "none";
+// });
+
+// Change Images onClick using JavaScript
+
+// const imageContainer = document.querySelector(".image_container");
+// const image = Array.from(document.querySelectorAll(".image"));
+// image.forEach((img) => img.addEventListener("click", (e) => {
+//     let imgSrc = e.target.getAttribute("src");
+//     console.log(imgSrc);
+//     imageContainer.setAttribute("src", `${imgSrc}`)
+// }));
+
+
+
+// Change Background Image Onclick using JavaScript
+
+// const imageContainer = document.querySelector(".imageContainer");
+// const image = Array.from(document.querySelectorAll(".image"));
+// image.forEach((img) => img.addEventListener("click", (e) => {
+//     let imgSrc = e.target.getAttribute("src");
+//     console.log(imgSrc);
+//     imageContainer.style.background = `url(${imgSrc})`
+// }));
+
+
+// Slider Images with Content onclick using JavaScript
+
+// var slides = document.querySelectorAll(".slide");
+// var btns = document.querySelectorAll(".btn");
+// let currentSlid = 1;
+
+// var manualNav = function (manual) {
+//     slides.forEach((slide) => {
+//         slide.classList.remove("active");
+//     })
+//     btns.forEach((btn) => {
+//         btn.classList.remove("active");
+//     })
+
+
+//     slides[manual].classList.add("active");
+//     btns[manual].classList.add("active");
+// }
+
+// btns.forEach((btn, i) => {
+//     btn.addEventListener("click", () => {
+//         manualNav(i);
+//         currentSlid = i;
+//     })
+// })
+
+
+// Toggle Menu Onclick using JavaScript
+
+
